4e577a04094564b50cd193413520e4f68121b605
Pabilito <pborsukiewicz@onet.pl>
2022-06-12 19:01:01 +0200

    Comments

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import numpy as np
 
-#Bernoulli distribution - returns 1 with probability p, and 0 with probability 1-p
 def bernoulli(p):
+    #Bernoulli distribution - returns 1 with probability p, and 0 with probability 1-p
     return(int(np.random.random()<p))
 
 class State:
+    #Class to store count of occurrances of a particular state (combination of result of X and Y)
     def __init__(self, Y, X, probability):
         self.Y = Y
         self.X = X
@@ -14,10 +15,13 @@ class State:
         self.counter += 1
 
 def getProbabilityOfY(probability_X, state_T_T, state_T_F):
+    #Retruns probability of test being positive
     probability_y_true = state_T_T.probability  * probability_X + state_T_F.probability * (1-probability_X)
     return probability_y_true
 
 def getInitialStates():
+    #Test_Cancer
+    #We have slightly modified values, because the ones in the task did not add up to 1 properly
     T_T = State(True, True, 0.9)
     T_F = State(True, False, 0.2)
     F_T = State(False, True, 0.1)
@@ -25,6 +29,7 @@ def getInitialStates():
     return T_T, T_F, F_T, F_F
 
 def createListOfStates():
+    #We define 4 states (confusion matrix)
     states = []
     T_T, T_F, F_T, F_F = getInitialStates()
     states.append(T_T)
@@ -34,18 +39,21 @@ def createListOfStates():
     return states
 
 def getState(states, Y, X):
+    #Returns one of four states
     for s in states:
         if s.X == X and s.Y == Y:
             return s
     return None #error - no such state
 
 def drawRandomState(states, p_y):
+    #Within the first iteration we want to perform a random choice of the first step
     Y = bernoulli(p_y)
     X = bernoulli(0.05)
     s = getState(states, Y, X)
     return s
 
 def changeState(states, current_state, change_X, p_y):
+    #Get a new state within our random walk
     s = current_state
     if change_X:
         new_X = bernoulli(0.05)
@@ -63,8 +71,11 @@ def randomWalk(states, current_state, p_y):
 def main(k):
     states = createListOfStates()
     probability_of_y = getProbabilityOfY(0.05, states[0], states[1])
+    #We start with selecting a ranndom state based on probabilities
     current_state = drawRandomState(states, probability_of_y)
+    #Then we save its result
     current_state.update()
+    #Further, we perform k steps of random walk
     for _ in range(k):
         current_state = randomWalk(states, current_state, probability_of_y)
 
@@ -74,4 +85,4 @@ def main(k):
     for s in states:
         print("State (Test, Cancer):", s.Y, s.X, "the probability: ", round(s.counter / (k+1), 5))
 
-main(10000)
+main(int(input("Please select number of steps: ")))
